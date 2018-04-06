c82614693804026103812f03b7f4b74bc842de9f
Jason Zaugg <jzaugg@gmail.com>
2018-04-06 14:42:30 +1000

    Found the right incantation of patch-module

--- README.md
+++ README.md
@@ -31,13 +31,10 @@
 ++ find src/main/java/example.a -name '*.java'
 + javac -d target/classes src/main/java/example.a/example/a/internal/Public.java src/main/java/example.a/example/a/MainRun.java src/main/java/example.a/example/a/Protected.java src/main/java/example.a/module-info.java
 ++ find src/test/java/example.a -name '*.java'
-+ javac --add-modules=java.xml --add-reads example.a=java.xml --module=example.a --module-source-path=src/main/java:src/test/java -d target/test-classes --module-path=target/classes src/test/java/example.a/example/a/Test.java
++ javac --add-modules=java.xml --add-reads example.a=java.xml -d target/test-classes --patch-module example.a=src/test/java/example.a --module-path=target/classes src/test/java/example.a/example/a/Test.java
 + java --module-path target/classes --module example.a/example.a.MainRun
 MainRun
-+ [[ -f target/test-classes/example.a/example/a/Test.class ]]
-+ java --add-modules=java.xml --add-reads example.a=java.xml --module-path target/classes --patch-module example.a=target/test-classes/example.a --module example.a/example.a.Test
-WARNING: module-info.class ignored in patch: target/test-classes/example.a
++ [[ -f target/test-classes/example/a/Test.class ]]
++ java --add-modules=java.xml --add-reads example.a=java.xml --module-path target/classes --patch-module example.a=target/test-classes --module example.a/example.a.Test
 Test
 ```
-
-Why does `test-classes` contain a copy `module-info.class`? Looks like we recompiled that by specifying `--module-source-path`.
--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ mkdir -p target/classes target/test-classes
 # Compile main classes into module 'example.a'
 javac -d target/classes $(find src/main/java/example.a -name '*.java')
 
+
 # Compile test classes, also into module 'example.a'
 # Adds an extra module used only by the tests
 # These can access non-exported packages from above, and package protected classes.
 javac --add-modules=java.xml                               \
       --add-reads example.a=java.xml                       \
-      --module=example.a                                   \
-      --module-source-path='src/main/java:src/test/java'   \
       -d target/test-classes                               \
+      --patch-module example.a=src/test/java/example.a     \
       --module-path=target/classes                         \
       $(find src/test/java/example.a -name '*.java')
 
@@ -24,15 +24,12 @@ java  --module-path target/classes                           \
       --module example.a/example.a.MainRun
 
 
-[[ -f "target/test-classes/example.a/example/a/Test.class" ]] || (echo "Test.class absent"; exit 1)
+[[ -f "target/test-classes/example/a/Test.class" ]] || (echo "Test.class absent"; exit 1)
 
 # Run a class from target/test-classes
 #
 java  --add-modules=java.xml                                 \
       --add-reads example.a=java.xml                         \
       --module-path target/classes                           \
-      --patch-module example.a=target/test-classes/example.a \
+      --patch-module example.a=target/test-classes           \
       --module example.a/example.a.Test
-#
-# FAIL: Error: Could not find or load main class example.a.Test in module example.a
-#
